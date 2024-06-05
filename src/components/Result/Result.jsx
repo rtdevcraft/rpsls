96f2f516e3514391e1e3600db1a4ba65524e0dc9
hiiamabat <hiiamabat@gmail.com>
2024-06-05 17:34:36 -0600

    Last upload wrap-up

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -46,45 +46,46 @@ const Result = ({ userChoice, computerChoice, result, onPlayAgain }) => {
 
   return (
     <section className="result">
-      <div className="user-result">
-        <div
-          className={`result-container ${userName} ${
-            isWinner && choicesRevealed ? "reveal-ripple" : ""
-          } ${choicesRevealed ? "show" : ""}`}
-        >
-          <div className={`choice-result ${userName}`}>
-            <img src={userIcon} alt={userName} />
-          </div>
-        </div>
-        <h3>You Picked</h3>
-      </div>
       <div
-        className={`computer-result show-placeholder ${
-          choicesRevealed ? "show" : ""
+        className={`choices-container ${
+          outcomeRevealed ? "reveal-outcome" : ""
         }`}
       >
-        <div
-          className={`result-container ${computerName} ${
-            isLoser && choicesRevealed ? "reveal-ripple" : ""
-          }`}
-        >
-          <div className={`choice-result ${computerName}`}>
-            <img src={computerIcon} alt={computerName} />
+        <div className="user-result">
+          <div
+            className={`result-container ${userName} ${
+              isWinner && choicesRevealed ? "reveal-ripple" : ""
+            } ${choicesRevealed ? "show" : ""}`}
+          >
+            <div className={`choice-result ${userName}`}>
+              <img src={userIcon} alt={userName} />
+            </div>
           </div>
+          <h3>You Picked</h3>
         </div>
-        <h3>The House Picked</h3>
+        {choicesRevealed && (
+          <div className={`outcome-container ${outcomeRevealed ? "show" : ""}`}>
+            <div className="outcome-text">
+              <h2>{result}</h2>
+              <button className="play-again-btn" onClick={onPlayAgain}>
+                Play Again
+              </button>
+            </div>
+          </div>
+        )}
+        <div className={`computer-result ${choicesRevealed ? "show" : ""}`}>
+          <div
+            className={`result-container ${computerName} ${
+              isLoser && choicesRevealed ? "reveal-ripple" : ""
+            }`}
+          >
+            <div className={`choice-result ${computerName}`}>
+              <img src={computerIcon} alt={computerName} />
+            </div>
+          </div>
+          <h3>The House Picked</h3>
+        </div>
       </div>
-
-      {choicesRevealed && (
-        <div className={`outcome-container ${outcomeRevealed ? "show" : ""}`}>
-          <div className="outcome-text">
-            <h2>{result}</h2>
-            <button className="play-again-btn" onClick={onPlayAgain}>
-              Play Again
-            </button>
-          </div>
-        </div>
-      )}
     </section>
   );
 };
